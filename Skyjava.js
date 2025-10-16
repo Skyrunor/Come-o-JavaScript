8450cce04560463d291c8dd7b7c1fedbe422ba9c
João Victor de Sousa Silva <joao.silva99460@edu.pe.senac.br>
2025-10-15 21:16:25 -0300

    Add files via upload

--- Skyjava.js
+++ Skyjava.js
@@ -1,11 +1,12 @@
 let contador = 0
-let res = document.querySelector(`section#result`)
+let res = document.querySelector('section#result')
 
 function contador(){
     contador++
-    res.innerHTML = `<p>O contador está com <mark>${contador}</mark> Cliques.</p>`
+    res.innerHTML = `<p>O contador est[a com <mark>${contador}</mark>Cliques</p>`
+
 }
 function zerar(){
     contador = 0
-    res.innerHTML = null
+    res.innerHTMl = null
 }
