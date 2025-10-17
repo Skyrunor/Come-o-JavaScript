6344c8a25381dc9c03da54f4df3dceaa6fe13b2e
João Victor de Sousa Silva <joao.silva99460@edu.pe.senac.br>
2025-10-16 21:22:10 -0300

    Add files via upload

---
+++ skyJayVa.js
@@ -0,0 +1,16 @@
+let nome, nota1, nota2
+
+
+function clique(){
+    nome = prompt("Digite seu nome")
+    nota1 = parseFloat(prompt("primeira nota";))
+    nota2 = parseFloat(prompt("Segunda nota";))
+
+    media = (nota1 + nota2) / 2
+    if(media <= 5){
+     
+        let ()
+    }
+    document.write(`A media de ${nome} foi ${media}`)
+    document.write(if {media =>})
+}
